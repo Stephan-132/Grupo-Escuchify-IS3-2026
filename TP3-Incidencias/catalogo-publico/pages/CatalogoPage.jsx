dea22586a996d8e989731f62b35987d6fa99a913
Taikaros <kyasutora@gmail.com>
2026-05-10 17:31:04 -0300

    feat: implementar listado público de eventos (#22)

---
+++ TP3-Incidencias/catalogo-publico/pages/CatalogoPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CatalogoList from "../components/CatalogoList";
+
+const CatalogoPage = () => {
+    return (
+        <div className="catalogo-page">
+            <h1>Catálogo de Eventos</h1>
+            <CatalogoList />
+        </div>
+    );
+};
+
+export default CatalogoPage;
